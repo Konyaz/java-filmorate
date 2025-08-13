c47715f918415866d378a55a2d82eff023a5f022
Konyaz <fvp@pkzdrav.ru>
2025-08-14 00:47:17 +0300

    Fix: отладка тестов

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,10 +13,18 @@ import java.util.Set;
 @Data
 public class Film {
     private Long id;
+
+    @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
+
+    @Size(max = 200, message = "Описание не может быть длиннее 200 символов")
     private String description;
+
     private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность фильма должна быть положительной")
     private int duration;
+
     private Mpa mpa;
     private List<Genre> genres = new ArrayList<>();
     private Set<Long> likes;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -9,11 +10,29 @@ import java.util.Set;
 @Data
 public class User {
     private Long id;
+
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Электронная почта должна быть валидной")
     private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
     private String login;
+
     private String name;
+
+    @NotNull(message = "Дата рождения обязательна")
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
+
     private Set<Long> friends = new HashSet<>();
+
+    public String getName() {
+        if (name == null || name.isBlank()) {
+            return login;
+        }
+        return name;
+    }
 
     public void addFriend(Long friendId) {
         friends.add(friendId);
