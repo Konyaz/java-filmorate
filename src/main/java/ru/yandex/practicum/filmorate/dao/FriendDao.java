b279b40fb67aa2c8879e84b47c1f94bb110a42cb
Konyaz <fvp@pkzdrav.ru>
2025-08-15 20:57:30 +0300

    Fix: отладка тестов

--- src/main/java/ru/yandex/practicum/filmorate/dao/FriendDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FriendDao.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 public interface FriendDao {
     void addFriend(Long userId, Long friendId, String status);
+
     void confirmFriend(Long userId, Long friendId);
+
     void removeFriend(Long userId, Long friendId);
+
     List<Long> getFriends(Long userId);
+
     List<Long> getCommonFriends(Long userId, Long otherId);
 }
