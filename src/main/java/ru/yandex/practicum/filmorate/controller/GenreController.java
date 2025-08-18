9dbeeaceba5eb743de7c43e70b181d32e6dd91be
Konyaz <fvp@pkzdrav.ru>
2025-08-18 18:30:09 +0300

    Fix: Добавлен сервисный слой для Genre, рефакторинг GenreController

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.dao.GenreDao;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.service.GenreService;
 
 import java.util.List;
 
@@ -15,16 +14,15 @@ import java.util.List;
 @RequestMapping("/genres")
 @RequiredArgsConstructor
 public class GenreController {
-    private final GenreDao genreDao;
+    private final GenreService genreService;
 
     @GetMapping
     public List<Genre> getAllGenres() {
-        return genreDao.getAllGenres();
+        return genreService.getAllGenres();
     }
 
     @GetMapping("/{id}")
     public Genre getGenreById(@PathVariable Long id) {
-        return genreDao.getGenreById(id)
-                .orElseThrow(() -> new NotFoundException("Жанр с ID " + id + " не найден"));
+        return genreService.getGenreById(id);
     }
 }
