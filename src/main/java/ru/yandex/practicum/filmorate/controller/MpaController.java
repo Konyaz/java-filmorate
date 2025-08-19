f720d609191b400736657ec97eea433bfdffbfea
Konyaz <fvp@pkzdrav.ru>
2025-08-19 11:47:47 +0300

    Fix: Исправил MpaController и MpaServiceImpl для использования сервисного слоя

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.dao.MpaDao;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.MpaService;
 
 import java.util.List;
 
@@ -15,16 +14,15 @@ import java.util.List;
 @RequestMapping("/mpa")
 @RequiredArgsConstructor
 public class MpaController {
-    private final MpaDao mpaDao;
+    private final MpaService mpaService;
 
     @GetMapping
     public List<Mpa> getAllMpa() {
-        return mpaDao.getAllMpa();
+        return mpaService.getAllMpa();
     }
 
     @GetMapping("/{id}")
     public Mpa getMpaById(@PathVariable Long id) {
-        return mpaDao.getMpaById(id)
-                .orElseThrow(() -> new NotFoundException("Рейтинг MPA с ID " + id + " не найден"));
+        return mpaService.getMpaById(id);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -1,35 +1,11 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.dao.MpaDao;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.util.List;
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class MpaService {
-    private final MpaDao mpaDao;
+public interface MpaService {
+    List<Mpa> getAllMpa();
 
-    /**
-     * Получение списка всех рейтингов MPA
-     */
-    public List<Mpa> getAllMpa() {
-        log.info("Получение списка всех рейтингов MPA");
-        return mpaDao.getAllMpa();
-    }
-
-    /**
-     * Получение рейтинга MPA по ID
-     * @throws NotFoundException если рейтинг не найден
-     */
-    public Mpa getMpaById(long id) {
-        log.info("Получение рейтинга MPA с ID = {}", id);
-        return mpaDao.getMpaById(id)
-                .orElseThrow(() -> new NotFoundException("Рейтинг MPA с ID " + id + " не найден"));
-    }
+    Mpa getMpaById(Long id);
 }
