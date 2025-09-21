dc2adb7e5adc18740298a584954549738cd35ea7
Roman <roma.chekalin@gmail.com>
2025-09-21 13:13:30 +0300

    Sprint #13 (Add reviews) - Initial for pull request

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/mapper/ReviewRowMapper.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.service.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class ReviewRowMapper implements RowMapper<Review> {
+    @Override
+    public Review mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Review review = new Review();
+
+        review.setId(resultSet.getLong("id"));
+        review.setContent(resultSet.getString("content"));
+        review.setIsPositive(resultSet.getBoolean("is_positive"));
+        review.setFilmId(resultSet.getLong("film_id"));
+        review.setUserId(resultSet.getLong("user_id"));
+        review.setUseful(resultSet.getInt("useful"));
+
+        return review;
+    }
+}
